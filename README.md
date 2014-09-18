eed24150fd6718da5b64e37270c477b7f5de1171
mishra <tech@peoplevendor.com>
2014-09-18 13:04:35 -0700

    asdf

---
+++ README.md
@@ -0,0 +1,12 @@
+#Instructions
+
+--Read about REST - wikipedia
+  
+  RESTful web services cookbook - 
+  
+  GET - get information
+  POST - create 
+  PUT - update
+  DELETE - delete
+  
+  http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
-var candidates = [{id: 32, name: 'Ray'}, {id: 64, name: 'mishra'}];
+var data = require('../contacts').candidates;
+var uuid = require('uuid');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,18 +10,64 @@ router.get('/', function(req, res) {
 
 
 router.get('/candidates', function(req, res) {
-    console.log(req.query);
-    res.json(200, { message: 'Hello ' + req.query.name + '!! How do you do?!' });
+    res.json(200, data);
+});
+
+router.get('/candidates/:id', function(req, res) {
+    var ids = data.map(function (candidate) {
+        return candidate.contactid;
+    });
+    var index = ids.indexOf(req.params.id);
+    if ( index === -1 ) {
+        res.json(404, { message: 'data does not exist' });
+    } else {
+        res.json(200, data[index]);
+    }
 });
 
 router.post('/candidates', function (req, res) {
-    res.json(200, req.body);
+    req.body.contactid = uuid.v4();
+    data.push(req.body);
+    res.json(200, data[data.length - 1]);
 });
 
-router.get('/candidates/:index', function(req, res) {
-    console.log(req.params.index);
-    res.json(200, { candidate: candidates[Number(req.params.index)] });
+router.put('/candidates/:id', function (req, res){
+    // 1db31e07-4559-4f97-8782-097702e06132
+    var ids = data.map(function (candidate) {
+        return candidate.contactid;
+    });
+    var index = ids.indexOf(req.params.id);
+    if (index === -1) {
+        res.json(404, { message: 'Could not find the candidate with id ' + req.candidate.id })
+    } else {
+        // update the candidate
+        var candidate = data[index];
+        for (var prop in req.body) {
+            console.log(prop);
+            console.log(candidate);
+            if (candidate.hasOwnProperty(prop)) {
+                candidate[prop] = req.body[prop];
+            }
+        }
+        // send a response
+        res.json(200, { message: 'successfully updated data' });
+    }
 });
+
+
+router.delete('/candidates/:id', function(req, res) {
+    var ids = data.map(function (candidate) {
+        return candidate.contactid;
+    });
+    var index = ids.indexOf(req.params.id);
+    if ( index === -1 ) {
+        res.json(404, { message: 'candidate does not exist' });
+    } else {
+        data.splice(index, 1);
+        res.json(200, { message: 'deleted candidate ' + req.params.id});
+    }
+});
+
 
 
 router.get('/candidatePage', function (req, res) {
